e71ce073895a4e3280ad701caa46b5500a127cc5
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-01-01 19:31:19 +0530

    init Query Builder

---
+++ src/lib/components/elements/query-builder-button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let variableName: string;
+
+    export let callbackAddVariableToContainer: (arg: string) => void;
+    export let callbackRemoveVariableFromContainer: (arg: string) => void;
+    let btnState = false;
+
+    function handleCallback() {
+        if (!btnState) {
+            callbackAddVariableToContainer(variableName);
+            btnState = true;
+        }
+        else if (btnState) {
+            callbackRemoveVariableFromContainer
+            btnState = false;
+        }
+    }
+
+    $: btnState;
+</script>
+
+<button class={btnState ? 'selected' : 'unselected'} on:click={handleCallback}>
+    {variableName}
+</button>
---
+++ src/lib/components/screens/AutoMLQueryBuilder.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { executeQueryTrainAutoML } from "$lib/scripts/fastAPI/fastapi-requests";
+
+    export let variableNames:string[]
+    export let AutoMLTasks:string[]
+    export let OptimizationMetrics:string[]
+
+    let selectedTarget:string
+    let selectedVariables:string[]
+    let selectedTask:string
+    let selectedOptmizationMeteric:string
+    let selectedTimeBudget:Number
+
+    function handleTrainCallback(){
+        executeQueryTrainAutoML(selectedVariables,selectedTarget,selectedTask,selectedOptmizationMeteric,selectedTimeBudget)
+    }
+</script>
+
+<select bind:value={selectedTask}>
+    {#each AutoMLTasks as task}
+        <option>{task}</option>
+    {/each}
+</select>
+
+<select bind:value={selectedOptmizationMeteric}>
+    {#each OptimizationMetrics as metric}
+        <option>{metric}</option>
+    {/each}
+</select>
+
+
