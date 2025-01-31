de4e39af1e62668d33ef08be32ca02b374cfe6f2
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-01-31 21:05:18 +0530

    add import data for dashboard

---
+++ src/lib/types/user/dashboard.d.ts
@@ -0,0 +1,9 @@
+import type {ChartTypeRegistry} from "chartjs/auto"
+
+export type chartConfig = {
+    table: string
+    series : string
+    x : string
+    y : string
+    type : ChartTypeRegistry
+}
