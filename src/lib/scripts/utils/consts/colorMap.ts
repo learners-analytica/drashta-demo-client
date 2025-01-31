6268e9d00d12ef16b9f02067411034df32aba425
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-01-31 21:14:56 +0530

    add color consts

---
+++ src/lib/scripts/utils/consts/colorMap.ts
@@ -0,0 +1,36 @@
+export const colorMapFill: Record<string, string> = {
+    'red': "rgba(255, 0, 0, 0.2)",
+    'orange': "rgba(255, 165, 0, 0.2)",
+    'yellow': "rgba(255, 255, 0, 0.2)",
+    'green': "rgba(0, 128, 0, 0.2)",
+    'blue': "rgba(0, 0, 255, 0.2)",
+    'purple': "rgba(128, 0, 128, 0.2)",
+    'cyan': "rgba(0, 255, 255, 0.2)",
+    'pink': "rgba(255, 192, 203, 0.2)",
+    'black': "rgba(0, 0, 0, 0.2)"
+}
+
+export const colorMapBorder: Record<string,string> = {
+    'red': "rgba(255, 0, 0, 1)",
+    'orange': "rgba(255, 165, 0, 1)",
+    'yellow': "rgba(255, 255, 0, 1)",
+    'green': "rgba(0, 128, 0, 1)",
+    'blue': "rgba(0, 0, 255, 1)",
+    'purple': "rgba(128, 0, 128, 1)",
+    'cyan': "rgba(0, 255, 255, 1)",
+    'pink': "rgba(255, 192, 203, 1)",
+    'black': "rgba(0, 0, 0, 1)"
+}
+
+export const mainColorList: string[] = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "cyan",
+    "pink",
+    "black"
+]
+
