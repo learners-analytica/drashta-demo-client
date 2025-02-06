75add4408ace83f62508ce6899cd94dae4dadfab
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-06 13:15:53 +0530

    add POST table Data

--- src/lib/scripts/services/request.bridge.ts
+++ src/lib/scripts/services/request.bridge.ts
@@ -2,28 +2,65 @@ import type { DataSeries, TableStructure } from "@learners-analytica/drashta-typ
 
 export namespace GET {
   export async function tableStructures(): Promise<TableStructure[]> {
-    const url = `${import.meta.env.VITE_ANALYTICS_PACKAGE_URL}/struct/`;
+    const url = `${import.meta.env.BRIDGE_SERVER}/struct/`;
     const res = await fetch(url, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
     });
-    const data = await res.json() as TableStructure[];
+    const data = (await res.json()) as TableStructure[];
     return data;
-  }
-
-  export async function tableData(): Promise<DataSeries[]> {
-    return [] as DataSeries[];
   }
 }
 
 export namespace POST {
-    export async function returnAggData(
-        table:string,
-        columns:string,
-        agg:string
-    ){
-
+  export async function returnAggData(
+    table: string,
+    columns: string,
+    agg: string
+  ): Promise<DataSeries[]> {
+    const url = `${import.meta.env.BRIDGE_SERVER}/agg-data/`;
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        table,
+        columns,
+        agg,
+      }),
+    });
+    const data = (await res.json()) as DataSeries[];
+    if (data) {
+      return data;
     }
+    console.error({
+      message: "No data returned",
+    });
+    return [] as DataSeries[];
+  }
+
+  export async function tableData(table: string): Promise<DataSeries[]> {
+    const url = `${import.meta.env.BRIDGE_SERVER}/get-data/`;
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        table,
+      }),
+    });
+    const data = (await res.json()) as DataSeries[];
+    if (data) {
+      return data;
+    }
+    console.error({
+      message: "No data returned",
+    });
+    return [] as DataSeries[];
+  }
 }
+
