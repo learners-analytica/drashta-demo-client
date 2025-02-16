d32a2eff8ca981929384ab09a791e7389edfe20c
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-16 11:39:43 +0530

    update libs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { PUBLIC_BRIDGE_SERVER } from '$env/static/public';
 	import { getTableColumnData } from '$lib/scripts/services/request.bridge';
+	import { MainColor } from '$lib/scripts/utils/consts/colorMap';
 	import CustomChart from '$lib/components/charts/custom-chart.svelte';
 </script>
 
@@ -9,6 +10,6 @@
 </style>
 
 
-<CustomChart chartId='chart-1' chartType='bar' table='expense_request' column_x='expense_amount' column_y='expense_name'></CustomChart>
-<CustomChart chartId='chart-2' chartType='line' table='expense_request' column_x='expense_amount' column_y='expense_name'></CustomChart>
-<CustomChart chartId='chart-3' chartType='bar' table='expense_request' column_x='expense_amount' column_y='expense_name'></CustomChart>
+<CustomChart chartId='chart-1' chartType='bar' table='expense_request' column_x='expense_amount' column_y='expense_name' color_key={MainColor.Red}></CustomChart>
+<CustomChart chartId='chart-2' chartType='line' table='expense_request' column_x='expense_amount' column_y='expense_name' color_key={MainColor.Black}></CustomChart>
+<CustomChart chartId='chart-3' chartType='bar' table='expense_request' column_x='expense_amount' column_y='expense_name' color_key={MainColor.Blue}></CustomChart>
