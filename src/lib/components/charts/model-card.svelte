8acda326789732631c0925023a0bf820e9cc1725
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-24 21:26:16 +0530

    update model card

--- src/lib/components/charts/model-card.svelte
+++ src/lib/components/charts/model-card.svelte
@@ -1,3 +1,14 @@
 <script lang="ts">
     
 </script>
+
+<div>
+    <div class="model-card header-div">
+        <h1></h1>
+        <img>
+    </div>
+    <div class="model-card body-div">
+        <p></p>
+    </div>
+    <div class="model-card footer-div"></div>
+</div>
