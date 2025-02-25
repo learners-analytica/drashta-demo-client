4a527d734d723069fd6aef7a2ae3095df2e9d451
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-25 20:06:55 +0530

    init model-card

---
+++ src/lib/components/containers/colored-tag.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let content:string;
+    export let bgcolor:string;
+    export let color:string;
+</script>
+
+<div style="background-color: {bgcolor}; color: {color}">
+    <span >{content}</span>
+</div>
---
+++ src/lib/components/elements/model-card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { TModelMetadata } from "@learners-analytica/drashta-types-ts";
+    import ColoredTag from "../containers/colored-tag.svelte";
+    export let modelMetadata:TModelMetadata
+</script>
+
+<div>
+    <div class="model-header">
+        <h1>{modelMetadata.name}</h1>
+        <img class={modelMetadata.estimator} alt={modelMetadata.estimator}>
+    </div>
+    <div class="model-targets-holder">
+        <ColoredTag content={modelMetadata.target} bgcolor="#FF6384" color="#fff"></ColoredTag>
+    </div>
+</div>
