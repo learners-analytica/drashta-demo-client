b3520467f2cd68485690bfea13a2c945d9a43966
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-10 16:27:06 +0530

    update bridge requests

--- src/lib/scripts/services/request.bridge.ts
+++ src/lib/scripts/services/request.bridge.ts
@@ -1,8 +1,9 @@
 import type { TDataSeriesRaw } from "@learners-analytica/drashta-types-ts";
-import {PUBLIC_BRIDGE_SERVER} from '$env/static/public';
-export async function getTableColumnData(table: string, column: string | null = null, size:number = 100): Promise<TDataSeriesRaw> {
+import { PUBLIC_BRIDGE_SERVER } from '$env/static/public';
+
+export async function getTableColumnData(table: string, column: string | null = null, size: number = 100): Promise<TDataSeriesRaw> {
     const url = `http://${PUBLIC_BRIDGE_SERVER}/supabase/get-table-data/`;
-    const body = { table:table, column:column, size:size };
+    const body = { table: table, column: column, size: size };
     const response = await fetch(url, {
         method: "POST",
         headers: {
