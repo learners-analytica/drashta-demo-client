f56a08ff42d32f4d83afdaeb05b1cab438418ae9
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-11 14:30:34 +0530

    update dashboard element

---
+++ src/lib/components/elements/dashboard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import CustomChart from '$lib/components/elements/custom-chart.svelte';
+    import type { TVisualizationDefinition } from "$lib/types/user/dashboard";
+    export let chartDescriptions: TVisualizationDefinition
+</script>
+
+{#each chartDescriptions as chartDescription}
+    <CustomChart chartConfig={chartDescription}></CustomChart>
+{/each}
--- src/lib/components/screens/dashboard.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    import type { chartDescription } from "$lib/types/user/dashboard";
-	import CustomChart from "../charts/custom-chart.svelte";
-    export let chartDescriptions: chartDescription[]
-</script>
-
