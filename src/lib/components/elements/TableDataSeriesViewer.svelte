d3276cd0855382c387c457d07602f5c797c794c7
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-11 14:46:38 +0530

    add Table Data series viewer

---
+++ src/lib/components/elements/TableDataSeriesViewer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import DataSeriesCard from './DataSeriesCard.svelte';
+    import type { TTableMetaData } from "@learners-analytica/drashta-types-ts";
+
+    export let TableMetaData:TTableMetaData
+</script>
+
+{#each TableMetaData.table_data_series as data_series}
+    <DataSeriesCard columnMetaData={data_series}></DataSeriesCard>
+{/each}
