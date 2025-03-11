9fa7052a3793108ab5a3cd2133a59c3ecef7a4ae
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-11 14:23:30 +0530

    update Model Query

--- src/lib/components/screens/ModelQuery.svelte
+++ src/lib/components/screens/ModelQuery.svelte
@@ -1,8 +1,33 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import ColumnButton from './../elements/column-button.svelte';
+
+    import type { TModelQueryAddVariableResponse } from "$lib/types/utils/misc";
+    import { getTableStructure } from "$lib/scripts/services/request.bridge";
 
     export let table:string;
-    onMount(
-        
-    )
+    
+    let selectedColumns:string[] = []
+
+    $: selectedColumns = []
+
+    function addVariableToSelectedColumns(variable:string):TModelQueryAddVariableResponse{
+        if(selectedColumns.includes(variable)){
+            selectedColumns.splice(selectedColumns.indexOf(variable),1)
+            return {variableAdded:false,targetVariable:false}
+        }
+        else{
+            selectedColumns.push(variable)
+            let isTarget =( selectedColumns.indexOf(variable) == 0)
+            return {variableAdded:true,targetVariable:isTarget}
+        }
+    }
+
 </script>
+
+{#await getTableStructure(table)}
+    ...Loading Column Buttons
+{:then data} 
+    {#each data.table_column_head_data as data_series}
+        <ColumnButton columnName={data_series.column_name} callbackModelQueryAddVariable={addVariableToSelectedColumns}></ColumnButton>
+    {/each}
+{/await}
