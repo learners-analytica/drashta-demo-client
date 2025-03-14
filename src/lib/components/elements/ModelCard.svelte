d934470511fb10ee28c991dee9c08da9e2e4c2f2
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-14 15:52:42 +0530

    update modelCard

--- src/lib/components/elements/ModelCard.svelte
+++ src/lib/components/elements/ModelCard.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
-	import type { TModelMetadata } from '@learners-analytica/drashta-types-ts';
-	import ColoredTag from '../containers/ColoredTag.svelte';
-	export let modelMetadata: TModelMetadata;
+	import type { TDataSeriesMinimal, TModelMetadata } from '@learners-analytica/drashta-types-ts';
+	import IconColoredTag from '../containers/IconColoredTag.svelte';
+	export let modelMetadata: TModelMetadata = JSON.parse(JSON.stringify({}));
+	export let callback: (arg: string) => void;
 </script>
 
-<button>
+<button on:click={() => callback(modelMetadata.id)}>
 	<div>
+		{console.log(modelMetadata)}
 		<div class="model-header">
-			<h1>{modelMetadata.name}</h1>
-			<img class={modelMetadata.estimator} alt={modelMetadata.estimator} />
+			<span>
+				<h1>{modelMetadata.name}</h1>
+				<img class={modelMetadata.estimator} alt={modelMetadata.estimator} />
+			</span>
 		</div>
 		<div class="model-targets-holder">
-			<ColoredTag content={modelMetadata.target} bgcolor="#FF6384" color="#fff"></ColoredTag>
+			{#each modelMetadata.target as targets}
+				<IconColoredTag
+					content={targets.column_name}
+					bgcolor="#36A2EB"
+					color="#fff"
+					iconCode={targets.column_type}
+				></IconColoredTag>
+			{/each}
 		</div>
+		{#each modelMetadata.columns as feature}
+			<IconColoredTag
+				content={feature.column_name}
+				bgcolor="#36A2EB"
+				color="#fff"
+				iconCode={feature.column_type}
+			></IconColoredTag>
+		{/each}
 	</div>
 </button>
