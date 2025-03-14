0a076af7ad4c49f99da4db233dc797004bd80457
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-14 07:29:45 +0530

    update TableDataSeriesViewer

--- src/lib/components/elements/TableDataSeriesViewer.svelte
+++ src/lib/components/elements/TableDataSeriesViewer.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import DataSeriesCard from './DataSeriesCard.svelte';
     import type { TTableMetaData } from "@learners-analytica/drashta-types-ts";
-
     export let TableMetaData:TTableMetaData
 </script>
 
+<button><h1>{TableMetaData.table_name}</h1></button>
+
+<div>
 {#each TableMetaData.table_data_series as data_series}
     <DataSeriesCard columnMetaData={data_series}></DataSeriesCard>
 {/each}
+</div>
