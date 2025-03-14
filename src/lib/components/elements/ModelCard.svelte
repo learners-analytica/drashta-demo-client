fd7f78d7eee8c36c56629b56ae4c4e45c09686d1
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-14 07:30:57 +0530

    update model card

--- src/lib/components/elements/ModelCard.svelte
+++ src/lib/components/elements/ModelCard.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-    import type { TModelMetadata } from "@learners-analytica/drashta-types-ts";
-    import ColoredTag from "../containers/ColoredTag.svelte";
-    export let modelMetadata:TModelMetadata
+	import type { TModelMetadata } from '@learners-analytica/drashta-types-ts';
+	import ColoredTag from '../containers/ColoredTag.svelte';
+	export let modelMetadata: TModelMetadata;
 </script>
 
-<div>
-    <div class="model-header">
-        <h1>{modelMetadata.name}</h1>
-        <img class={modelMetadata.estimator} alt={modelMetadata.estimator}>
-    </div>
-    <div class="model-targets-holder">
-        <ColoredTag content={modelMetadata.target} bgcolor="#FF6384" color="#fff"></ColoredTag>
-    </div>
-</div>
+<button>
+	<div>
+		<div class="model-header">
+			<h1>{modelMetadata.name}</h1>
+			<img class={modelMetadata.estimator} alt={modelMetadata.estimator} />
+		</div>
+		<div class="model-targets-holder">
+			<ColoredTag content={modelMetadata.target} bgcolor="#FF6384" color="#fff"></ColoredTag>
+		</div>
+	</div>
+</button>
