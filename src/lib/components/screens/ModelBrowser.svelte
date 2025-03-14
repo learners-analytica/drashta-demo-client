6f80071f832578f2b4acd24f55267774167458b7
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-14 15:52:49 +0530

    init model browser

---
+++ src/lib/components/screens/ModelBrowser.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { requestModelList } from "$lib/scripts/services/request.analytics";
+	import ModelCard from "../elements/ModelCard.svelte";
+
+    function handleModelClick(model_id:string){
+        console.log(model_id)
+    }
+</script>
+
+{#await requestModelList()}
+    ... Loading
+{:then modelList}
+    {#each modelList as model}
+        <ModelCard modelMetadata={model} callback={handleModelClick}></ModelCard>
+    {/each}
+{/await}
