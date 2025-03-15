0adf929b8beebbd35e273008a1af2954b1501b26
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-15 14:48:11 +0530

    add test callback to dataSeriesViewer

--- src/lib/components/elements/TableDataSeriesViewer.svelte
+++ src/lib/components/elements/TableDataSeriesViewer.svelte
@@ -2,12 +2,16 @@
 	import DataSeriesCard from './DataSeriesCard.svelte';
     import type { TTableMetaData } from "@learners-analytica/drashta-types-ts";
     export let TableMetaData:TTableMetaData
+
+    function callbackSystem(){
+        console.log(1)
+    }
 </script>
 
 <button><h1>{TableMetaData.table_name}</h1></button>
 
 <div>
 {#each TableMetaData.table_data_series as data_series}
-    <DataSeriesCard columnMetaData={data_series}></DataSeriesCard>
+    <DataSeriesCard columnMetaData={data_series} callback={callbackSystem}></DataSeriesCard>
 {/each}
 </div>
