ea7f1153ca215279c812856f832dbeb7cf8ece43
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-15 15:08:37 +0530

    update stores

--- src/lib/scripts/client/stores.ts
+++ src/lib/scripts/client/stores.ts
@@ -3,7 +3,7 @@ import type { Writable } from 'svelte/store';
 import { EMenu } from './consts';
 
 export const menu:Writable<EMenu> = writable(EMenu.Home);
-const model_id:Writable<string | undefined> = writable(undefined);
+const model_id:Writable<string|undefined> = writable("");
 const selected_columns:Writable<string[]> = writable([]);
 
 
@@ -11,4 +11,25 @@ const selected_columns:Writable<string[]> = writable([]);
 
 export function setMenu(value:EMenu):void{
     menu.set(value);
+}
+
+// Model ID
+
+export function setModelId(value:string):void{
+    model_id.set(value)
+}
+
+export function resetModelId():void{
+    model_id.set(undefined)
+}
+
+// Selected Columns
+
+function addColumn(newColumn:string):void{
+    let currentColumns = selected_columns.update()
+    currentColumns
+}
+
+function removeColumn(newColumn:string):void{
+
 }
