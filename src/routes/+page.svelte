5abd1e7f1d1da4f6351968ff620ef0b0e205c09e
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-16 11:14:36 +0530

    clear testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,4 @@
 <script lang="ts">
-	import { menu } from "$lib/scripts/client/stores";
-	import { EMenu } from "$lib/scripts/client/consts";
-	import { onMount } from "svelte";
-	import { loadDashboard, dashboard } from "$lib/scripts/client/stores";
-
-	onMount(async () => {
-		await loadDashboard();
-  	})
 </script>
 
-{#if $menu === EMenu.Home}
-	<div></div>
-{:else if  $menu === EMenu.Database}
-	<div></div>	
-{:else if  $menu === EMenu.Playground}
-	<div></div>	
-{:else if  $menu === EMenu.Models}
-	<div></div>	
-{:else if  $menu === EMenu.ModelQuery}
-	<div></div>	
-{/if}
+<DashboardScreen></DashboardScreen>
