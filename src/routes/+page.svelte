9ec5f3d16e28abb70d59e520ae8480929d59a213
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-16 13:25:47 +0530

    add test components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+    import { loadDashboard } from "$lib/scripts/client/dashboard";
+	import DashboardScreen from "$lib/components/screens/DashboardScreen.svelte";
+	import TableBrowser from "$lib/components/screens/TableBrowser.svelte";
 </script>
+
+{#await loadDashboard() then value}
+    <DashboardScreen></DashboardScreen>
+{/await}
+
+<TableBrowser></TableBrowser>
