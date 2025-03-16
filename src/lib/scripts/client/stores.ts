3923d88132dd652a29bd1f584e6092761be6b79f
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-16 10:17:55 +0530

    add dashboard doc

--- src/lib/scripts/client/stores.ts
+++ src/lib/scripts/client/stores.ts
@@ -1,10 +1,12 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 import { EMenu } from './consts';
+import type { TDashboardDefinitionDoc } from '$lib/types/user/dashboard';
+import { PUBLIC_DASHBOARD_DEFINITION_DOC } from "$env/static/public";
 
 export const menu:Writable<EMenu> = writable(EMenu.Home);
 export const model_id:Writable<string|undefined> = writable("");
-
+export const dashboard:Writable<TDashboardDefinitionDoc|undefined> = writable(undefined);
 
 // Menu
 
@@ -20,4 +22,26 @@ export function setModelId(value:string):void{
 
 export function resetModelId():void{
     model_id.set(undefined)
+}
+
+// Dashboard
+
+export function setDashboard(value:TDashboardDefinitionDoc):void{
+    dashboard.set(value);
+}
+
+async function setDashboardFromFile(path:string):Promise<void>{
+    const response = await fetch(`${path}`);
+    if (!response.ok) {
+        throw new Error(`Failed to load dashboard from file ${path}: ${response.statusText}`);
+    }
+    const data = await response.json();
+    setDashboard(data);
+}
+
+
+export async function loadDashboard():Promise<void>{
+    if(PUBLIC_DASHBOARD_DEFINITION_DOC !== undefined){
+        await setDashboardFromFile(PUBLIC_DASHBOARD_DEFINITION_DOC);
+    }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import { menu } from "$lib/scripts/client/stores";
 	import { EMenu } from "$lib/scripts/client/consts";
+	import { onMount } from "svelte";
+	import { loadDashboard, dashboard } from "$lib/scripts/client/stores";
+
+	onMount(async () => {
+		await loadDashboard();
+  	})
 </script>
 
 {#if $menu === EMenu.Home}
