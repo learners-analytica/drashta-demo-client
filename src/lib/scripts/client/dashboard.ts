2b8eabe20d611b7c2c81cfbdecb02565b17f30de
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-16 13:25:28 +0530

    add Add Chart script

--- src/lib/scripts/client/dashboard.ts
+++ src/lib/scripts/client/dashboard.ts
@@ -1,6 +1,9 @@
-import type { TDashboardDefinition, TDashboardEntry } from "$lib/types/user/dashboard";
-import { setDashboardFromFile, dashboard } from "./stores";
+import type { TDashboardDefinition, TDashboardEntry, ChartType } from "$lib/types/user/dashboard";
+import { setDashboardFromFile, setCurrentDashboardIndex } from "./stores";
 import { PUBLIC_DASHBOARD_DEFINITION_DOC } from "$env/static/public";
+import type { TCustomChartConfig, } from "$lib/types/user/dashboard";
+import { MainColor } from "../utils/consts/colorMap";
+import Chart from 'chart.js/auto';
 
 export function extractDashboardEntryList(Doc: TDashboardDefinition[]): TDashboardEntry[] {
     return Doc.map((dashboard, index) => ({
@@ -8,8 +11,31 @@ export function extractDashboardEntryList(Doc: TDashboardDefinition[]): TDashboa
         index
     }));
 }
+
 export async function loadDashboard():Promise<void>{
     if(PUBLIC_DASHBOARD_DEFINITION_DOC !== undefined){
         await setDashboardFromFile(PUBLIC_DASHBOARD_DEFINITION_DOC);
+    }
+}
+
+export function switchDashboardEntry(index:number):void{
+    setCurrentDashboardIndex(index);
+}
+
+export function addNewVisualizationConfiguration(
+    chart_type:ChartType,
+    source_table:string,
+    index_column:string,
+    data_column:string,
+    color_key:MainColor,
+    size:number
+):void{
+    const vizConf:TCustomChartConfig = {
+        chart_type: chart_type,
+        source_table: source_table,
+        index_column: index_column,
+        data_column: data_column,
+        color_key: color_key,
+        size:size
     }
 }
