c314eeaf504f47038d1a95827f1407dfe4e6011c
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-17 22:34:45 +0530

    update +page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { menu } from "$lib/scripts/client/stores";
-	import { EMenu } from "$lib/scripts/client/consts";
-    import { loadDashboard } from "$lib/scripts/client/dashboard";
-	import DashboardScreen from "$lib/components/screens/DashboardScreen.svelte";
-	import TableBrowser from "$lib/components/screens/TableBrowser.svelte";
-	import ModelBrowser from "$lib/components/screens/ModelBrowser.svelte";
+	import { onMount } from 'svelte';
+	import { menu } from '$lib/scripts/client/stores';
+	import { EMenu } from '$lib/scripts/client/consts';
+	import { loadDashboard } from '$lib/scripts/client/dashboard';
+	import DashboardScreen from '$lib/components/screens/DashboardScreen.svelte';
+	import TableBrowser from '$lib/components/screens/TableBrowser.svelte';
+	import ModelBrowser from '$lib/components/screens/ModelBrowser.svelte';
+	import ModelQuery from '$lib/components/screens/ModelQuery.svelte';
 </script>
 
 {#if $menu}
 	{#if $menu === EMenu.Home}
-	{#await loadDashboard}
-		...Loading Viz
-	{:then dashboard}
-		<DashboardScreen></DashboardScreen>
-	{/await}
-	{:else if  $menu === EMenu.Database}
-	<TableBrowser></TableBrowser>
-	{:else if  $menu === EMenu.ModelQuery}
-		 <!-- else if content here -->
-	{:else if  $menu === EMenu.Models}
-		 <ModelBrowser></ModelBrowser>
-	{:else if  $menu === EMenu.Playground}
-		 <!-- else if content here -->
+		{#await loadDashboard}
+			...Loading Viz
+		{:then dashboard}
+			<DashboardScreen></DashboardScreen>
+		{/await}
+	{:else if $menu === EMenu.Database}
+		<TableBrowser></TableBrowser>
+	{:else if $menu === EMenu.ModelQuery}
+		<ModelQuery></ModelQuery>
+	{:else if $menu === EMenu.Models}
+		<ModelBrowser></ModelBrowser>
+	{:else if $menu === EMenu.Playground}
+		<!-- else if content here -->
 	{:else}
-		 <!-- else content here -->
+		<!-- else content here -->
 	{/if}
 {/if}
