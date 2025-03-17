5d4219afa0aaa6ab9b95d0fe4dac34db2edab698
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-17 15:33:51 +0530

    update navbar

--- src/lib/components/menus/NavMenu.svelte
+++ src/lib/components/menus/NavMenu.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { EMenu, MENUS } from "$lib/scripts/client/consts";
+	import { setMenu } from "$lib/scripts/client/stores";
 </script>
 <div>
 	{#each MENUS as menu}
-		<button>{menu}</button>
+		<button on:click={() => setMenu(menu)}>{menu}</button>
 	{/each}
 </div>
