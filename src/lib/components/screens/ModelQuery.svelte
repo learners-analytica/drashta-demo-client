e86477265e5f46e4d8b0fcf5b10ba9660079977e
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-18 23:15:01 +0530

    update model query

--- src/lib/components/screens/ModelQuery.svelte
+++ src/lib/components/screens/ModelQuery.svelte
@@ -6,23 +6,46 @@
     import { currentTable } from '$lib/scripts/client/stores';
 	import { onMount } from 'svelte';
     import { MLTaskTypes } from '@learners-analytica/drashta-types-ts';
+    import { requestModelCreation } from '$lib/scripts/services/request.analytics';
+    import { setMenu } from '$lib/scripts/client/stores';
+    import { EMenu } from '$lib/scripts/client/consts';
     
     let selectedColumns:string[] = []
+    let model_name:string = ""
+    let model_task:MLTaskTypes = MLTaskTypes.REGRESSION
 
     $: selectedColumns = []
+    $: model_name
+    $: model_task
 
     function addVariableToSelectedColumns(variable:string):TModelQueryAddVariableResponse{
         if(selectedColumns.includes(variable)){
             selectedColumns.splice(selectedColumns.indexOf(variable),1)
+            console.log(selectedColumns)
             return {variableAdded:false,targetVariable:false}
         }
         else{
             selectedColumns.push(variable)
             let isTarget =( selectedColumns.indexOf(variable) == 0)
+            console.log(selectedColumns)
             return {variableAdded:true,targetVariable:isTarget}
         }
     }
 
+    async function handleSubmitModelQuery(){
+        const selectedColumnsInput = selectedColumns 
+        const y_columns = selectedColumnsInput[0]
+        selectedColumnsInput.shift()
+        const response = await requestModelCreation(
+            $currentTable,
+            selectedColumns,
+            y_columns,
+            model_name,
+            1000,
+            model_task
+        )
+        setMenu(EMenu.Models)
+    }
     onMount(() => {
         console.log($currentTable)
         console.log("On Model Query")
@@ -38,9 +61,9 @@
     {/each}
 {/await}
 
-<form>
-    <input type="text" placeholder="Model Name">
-    <select>
+<form on:submit={handleSubmitModelQuery}>
+    <input type="text" placeholder="Model Name" bind:value={model_name}>
+    <select bind:value={model_task}>
         {#each Object.values(MLTaskTypes) as taskType}
             <option>{taskType}</option>
         {/each}
