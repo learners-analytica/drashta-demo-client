0bb06a0f753a891fe7f04627cf5c42c6155e0467
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-18 23:15:12 +0530

    update playground

--- src/lib/components/screens/Playground.svelte
+++ src/lib/components/screens/Playground.svelte
@@ -2,13 +2,23 @@
     import { currentModelID } from "$lib/scripts/client/stores";
     import { requestModelDetails } from "$lib/scripts/services/request.analytics";
     import type { TDataArray } from "@learners-analytica/drashta-types-ts";
+	import { onMount } from "svelte";
+    import { TabulatorFull as Tabulator } from "tabulator-tables";
+
+
     let predictions:TDataArray[] = []
     $: predictions = []
+
+    onMount(async()=>{
+        
+    })
 </script>
 
 {#await requestModelDetails($currentModelID)}
     <!-- promise is pending -->
 {:then value}
-    {value.columns[0]}
+    {#each value.columns as column}
+        <p>{column.column_name}</p>
+    {/each}
     {console.log(value.columns)}
 {/await}
