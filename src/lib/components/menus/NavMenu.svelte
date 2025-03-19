517cfce7bc82fafa15807fb887b7dbb2ada615fa
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-19 12:08:45 +0530

    add styles to navbar

--- src/lib/components/menus/NavMenu.svelte
+++ src/lib/components/menus/NavMenu.svelte
@@ -2,8 +2,8 @@
 	import { EMenu, MENUS } from "$lib/scripts/client/consts";
 	import { setMenu } from "$lib/scripts/client/stores";
 </script>
-<div>
+<div class="flex flex-row justify-center content-between flex-2 shadow-md">
 	{#each MENUS as menu}
-		<button on:click={() => setMenu(menu)}>{menu}</button>
+		<button on:click={() => setMenu(menu)} class="m-1 p-2 shadow-sm rounded-sm">{menu}</button>
 	{/each}
 </div>
