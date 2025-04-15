5777171eeab02b7f112ccffa1fb350702c15ac4c
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-04-15 11:21:50 +0530

    update iconMaps

--- src/lib/components/containers/IconColoredTag.svelte
+++ src/lib/components/containers/IconColoredTag.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import ColoredTag from "./ColoredTag.svelte";
+    import { iconMapDataTypes } from "$lib/scripts/utils/consts/iconMaps";
     export let content:string;
     export let iconCode:string;
     export let bgcolor:string;
     export let color:string;
 </script>
 <ColoredTag content={content} bgcolor={bgcolor} color={color}>
-    <i class={iconCode}></i>
+    <i class={iconMapDataTypes[iconCode]}></i>
 </ColoredTag>
--- src/lib/scripts/utils/consts/iconMaps.ts
+++ src/lib/scripts/utils/consts/iconMaps.ts
@@ -1,7 +1,7 @@
 export const iconMapDataTypes: Record<string, string> = {
-    'free': 'bx bxs-calendar-check color-green',
-    'working-dev': 'bx bx-code-block color-blue',
-    'working-doc': 'bx bx-edit color-blue',
-    'late': 'bx bxs-time-five color-red',
+    'free': 'bx bxs-calendar-check',
+    'working-dev': 'bx bx-code-block',
+    'working-doc': 'bx bx-edit',
+    'late': 'bx bxs-time-five',
 };
 
